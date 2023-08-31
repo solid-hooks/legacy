7380e3957bd65c453d3f04f5174d8a5c4ded2029
subframe7536 <1667077010@qq.com>
2023-08-31 18:24:16 +0800

    feat: improve type in $watch

--- src/watch.ts
+++ src/watch.ts
@@ -1,25 +1,26 @@
 import type { Accessor, AccessorArray, OnOptions } from 'solid-js'
 import { createEffect, createSignal, on } from 'solid-js'
-
-import { type SignalObject } from './signal'
+import type { SignalObject } from './signal'
 
 /**
- * when using options.fileter, prevInput will fail to filter
+ * watch callback function
+ * @param value current value
+ * @param oldValue previous value
+ * @description when using options.filter, `prevInput` will fail to filter
  */
-export type WatchCallback<S, Next extends Prev = any, Prev = Next> = (
-  input: S,
-  prevInput: S | undefined,
-  prev: Prev
-) => void | Next | Promise<void | Next>
+export type WatchCallback<S> = (
+  value: S,
+  oldValue: S | undefined,
+) => void
 
 export type WatchOption<T> = OnOptions & {
   /**
    * function for trigger callback, like `debounce()` or `throttle()` in `@solid-primitives/scheduled`
-  */
-  callFn?: ((fn: (...args: unknown[]) => void) => void)
+   */
+  triggerFn?: (fn: WatchCallback<T>) => WatchCallback<T>
   /**
-  * function for filter value
-  */
+   * function for filter value
+   */
   filterFn?: (newValue: T, times: number) => boolean
 }
 
@@ -50,35 +51,31 @@ type WatchReturn = {
  * @param options options
  * @returns void
  */
-export function $watch<T, Next extends Prev, Prev = Next>(
+export function $watch<T>(
   deps: Accessor<T> | AccessorArray<T> | SignalObject<T>,
-  fn: WatchCallback<T, Next, Prev>,
+  fn: WatchCallback<T>,
   options: WatchOption<T> = {},
 ): WatchReturn {
   const [isWatch, setIsWatch] = createSignal(true)
   const [callTimes, setCallTimes] = createSignal(0)
-  const { callFn, defer = false, filterFn: filter } = options
+  const { triggerFn, defer = false, filterFn } = options
 
   const needToTriggerEffect = (newValue: T) => {
     return isWatch()
-      ? filter
-        ? filter(newValue, callTimes())
+      ? filterFn
+        ? filterFn(newValue, callTimes())
         : true
       : false
   }
-  createEffect(on(deps, (value, oldValue, old) => {
-    if (!needToTriggerEffect(value)) {
-      return
-    }
-    if (callFn) {
-      callFn(() => {
-        setCallTimes(time => time + 1)
-        fn(value, oldValue, old as any)
-      })
-    } else {
-      setCallTimes(time => time + 1)
-      fn(value, oldValue, old as any)
-    }
+  const _fn = triggerFn?.(() => {
+    setCallTimes(time => time + 1)
+    return fn
+  }) || ((value, oldValue) => {
+    setCallTimes(time => time + 1)
+    return fn(value, oldValue)
+  })
+  createEffect(on(deps, (value, oldValue) => {
+    needToTriggerEffect(value) && _fn(value, oldValue)
   }, { defer: defer as any }))
 
   return {
