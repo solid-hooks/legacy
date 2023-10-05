07916ddb642b6ab9e67e3ba6977a00edac6dd5e0
subframe7536 <1667077010@qq.com>
2023-10-05 20:08:57 +0800

    revert: resume deepClone using klona

--- src/state/utils.ts
+++ src/state/utils.ts
@@ -1,10 +1,19 @@
 import { batch, untrack } from 'solid-js'
+import { klona } from 'klona'
 import type { GetterOrActionObject } from './types'
+
+/**
+ * alias for {@link klona}
+ */
+export const deepClone = klona
 
 function batchAction<T extends (...args: any) => any>(fn: T): T {
   return ((...args) => batch(() => untrack(() => fn(...args)))) as T
 }
 
+/**
+ * @internal
+ */
 export function createActions<T extends GetterOrActionObject>(functions?: T): T {
   if (!functions) {
     return {} as T
